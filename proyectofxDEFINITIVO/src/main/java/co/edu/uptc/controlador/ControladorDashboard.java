ac05a5e3dbe8593f7716dc82396d8c0521332bbd
Davidcasstella <brayan.lopez07@uptc.edu.co>
2025-05-24 22:54:53 -0500

    Se añadio la pantalla principal

--- proyectofxDEFINITIVO/src/main/java/co/edu/uptc/controlador/ControladorDashboard.java
+++ proyectofxDEFINITIVO/src/main/java/co/edu/uptc/controlador/ControladorDashboard.java
@@ -2,68 +2,68 @@ package co.edu.uptc.controlador;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.ListView;
-
-import co.edu.uptc.persistencia.DonanteDAO;
-
-import co.edu.uptc.persistencia.DonacionDAO;
 
 public class ControladorDashboard {
 
     @FXML
-    private Label labelDonantes;
+    private Label labelDonantes, labelAnimales, labelDonaciones;
 
     @FXML
-    private Label labelAnimales;
+    private ListView<String> activityList;
 
     @FXML
-    private Label labelDonaciones;
+    private ProgressBar progressBueno, progressRegular, progressCritico;
 
     @FXML
-    private LineChart<String, Number> graficaResumen;
+    private Label labelBueno, labelRegular, labelCritico;
 
     @FXML
-    private ListView<String> listaActividadReciente;
+    private LineChart<Number, Number> lineChart;
 
+    @FXML
     public void initialize() {
-        cargarDatosDashboard();
-    }
+        // Inicializar estadísticas con valores simulados
+        labelDonantes.setText("10,353");
+        labelAnimales.setText("2,405");
+        labelDonaciones.setText("$45,678.90");
 
-    private void cargarDatosDashboard() {
-        // Obtener datos desde los DAOs
-        int totalDonantes = DonanteDAO.obtenerCantidadDonantes();
-       
-       
+        labelBueno.setText("50");
+        labelRegular.setText("30");
+        labelCritico.setText("10");
 
-        // Mostrar en labels
-        labelDonantes.setText(String.valueOf(totalDonantes));
-        
-        
+        progressBueno.setProgress(0.50);
+        progressRegular.setProgress(0.30);
+        progressCritico.setProgress(0.10);
 
-        // Cargar gráfica y actividad
-        cargarGrafica();
-        cargarActividadReciente();
-    }
+        // Limpiar y preparar la gráfica
+        lineChart.getData().clear();
+        lineChart.setAnimated(false);
+        lineChart.setCreateSymbols(false);
+        lineChart.setLegendVisible(false);
 
-    private void cargarGrafica() {
-        XYChart.Series<String, Number> serie = new XYChart.Series<>();
-        serie.getData().add(new XYChart.Data<>("23 Nov", 22000));
-        serie.getData().add(new XYChart.Data<>("24 Nov", 23000));
-        serie.getData().add(new XYChart.Data<>("25 Nov", 27000));
-        serie.getData().add(new XYChart.Data<>("29 Nov", 47000));
-        graficaResumen.getData().clear();  // limpiar antes de agregar
-        graficaResumen.getData().add(serie);
-    }
+        // Datos simulados para gráfico
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>(23, 25000));
+        series.getData().add(new XYChart.Data<>(24, 28000));
+        series.getData().add(new XYChart.Data<>(25, 32000));
+        series.getData().add(new XYChart.Data<>(26, 35000));
+        series.getData().add(new XYChart.Data<>(27, 33000));
+        series.getData().add(new XYChart.Data<>(28, 40000));
+        series.getData().add(new XYChart.Data<>(29, 42000));
+        series.getData().add(new XYChart.Data<>(30, 48000));
+        lineChart.getData().add(series);
 
-    private void cargarActividadReciente() {
-        listaActividadReciente.getItems().clear(); // limpiar antes de agregar
-        listaActividadReciente.getItems().addAll(
+        // Listado de actividades recientes (simulado)
+        activityList.getItems().clear();
+        activityList.getItems().addAll(
             "Nuevo donante registrado - Hace 5 min",
             "Asignación de medicamentos - Hace 1 hora",
             "Animal ingresado al sistema - Hace 3 horas",
-            "Reporte generado - Ayer",
+            "Reporte Generado - Ayer",
             "Donación confirmada - Hace 2 días"
         );
     }
